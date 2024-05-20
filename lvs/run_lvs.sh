4fd86e41b4f1db9dd4da1274586054ac2c1d01bf
Tim Edwards <tim@opencircuitdesign.com>
2024-05-19 21:46:35 -0400

    Initial commit of files.  This rheostat design is largely copied from the 8-bit resistive DAC, with the main differences being the removal of the follower amplifier at the end, and the change of resistor type and size to make the full end-to-end resistance 50kohms.

---
+++ lvs/run_lvs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+netgen -batch source ../cace/run_project_lvs.tcl | tee netgen.log
+
---
+++ lvs/run_lvs_flat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+netgen -batch source ../cace/run_project_lvs_flat.tcl | tee netgen.log
+
