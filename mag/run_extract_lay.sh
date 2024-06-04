08485cf26e9ca0477c88d72f5bb13ac0a54a0f1e
azwefabless <andrew.wright@efabless.com>
2024-06-03 22:33:47 -0400

    Signed-off-by: azwefabless <andrew.wright@efabless.com> Checking in with automation for running extraction for sch and layout from the command line as well as drc.Did not chekc the CACE automation.

---
+++ mag/run_extract_lay.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+magic -dnull -noconsole -rcfile $PDK_ROOT/$PDK/libs.tech/magic/sky130A.magicrc << EOF
+load sky130_ef_ip__rheostat_8bit
+select top cell
+extract path extfiles
+extract all
+ext2spice lvs
+ext2spice -p extfiles -o ../netlist/layout/sky130_ef_ip__rheostat_8bit_azwtest.spice
+quit -noprompt
+EOF
+rm -r extfiles
+exit 0
+
+
---
+++ xschem/run_extract_sch.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+xschem -n -s -r -x -q --tcl "set lvs_netlist 1" --rcfile $PDK_ROOT/$PDK/libs.tech/xschem/xschemrc -o ../netlist/schematic -N sky130_ef_ip__rheostat_8bit_testazw.spice sky130_ef_ip__rheostat_8bit.sch
